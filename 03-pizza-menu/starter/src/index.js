51f3542cd42c6aef7215e1354b16282722b3c252
unknown <polibos6366@gmail.com>
2024-09-25 18:59:25 -0600

    Rendering the Root Component and Strict Mode

---
+++ 03-pizza-menu/starter/src/index.js
@@ -0,0 +1,15 @@
+import React, { StrictMode } from "react";
+
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello React !</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
