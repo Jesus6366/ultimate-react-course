5b749606fd7b46f46210f3a6ca02f820e5187ed1
unknown <polibos6366@gmail.com>
2024-09-26 19:19:21 -0600

    Destructuring Props

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -103,16 +103,16 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  if (props.pizzaObject.soldOut) return null;
+function Pizza({ pizzaObject }) {
+  if (pizzaObject.soldOut) return null;
 
   return (
     <li className="pizza">
-      <img src={props.pizzaObject.photoName} alt={props.pizzaObject.name} />
+      <img src={pizzaObject.photoName} alt={pizzaObject.name} />
       <div>
-        <h3>{props.pizzaObject.name}</h3>
-        <p>{props.pizzaObject.ingredients}</p>
-        <span>${props.pizzaObject.price}</span>
+        <h3>{pizzaObject.name}</h3>
+        <p>{pizzaObject.ingredients}</p>
+        <span>${pizzaObject.price}</span>
       </div>
     </li>
   );
@@ -155,10 +155,10 @@ function Footer() {
   );
 }
 
-function Order(props) {
+function Order({ closeHour }) {
   return (
     <div className="order">
-      <p>We're open until {props.closeHour} Come visit us or order online.</p>
+      <p>We're open until {closeHour} Come visit us or order online.</p>
       <button className="btn">Order</button>
     </div>
   );
