444d5fde268a1da4dce6a54cefc82d6949105dd1
unknown <polibos6366@gmail.com>
2024-09-26 18:41:53 -0600

    Rendering Lists

--- 03-pizza-menu/starter/src/index.js
+++ 03-pizza-menu/starter/src/index.js
@@ -72,7 +72,7 @@ function Menu() {
   return (
     <main className="menu">
       <h2> Our Menu </h2>
-      <Pizza
+      {/* <Pizza
         name={"Pizza spinaci"}
         ingredients={"Tomato, mozarella, spinach, and ricotta cheese"}
         photoName={"pizzas/spinaci.jpg"}
@@ -84,21 +84,26 @@ function Menu() {
         ingredients={"Tomate, mushrooms"}
         price={12}
         photoName={"pizzas/funghi.jpg"}
-      />
+      /> */}
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza key={pizza.name} pizzaObject={pizza} />
+        ))}
+      </ul>
     </main>
   );
 }
 
 function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObject.photoName} alt={props.pizzaObject.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>${props.price}</span>
+        <h3>{props.pizzaObject.name}</h3>
+        <p>{props.pizzaObject.ingredients}</p>
+        <span>${props.pizzaObject.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
