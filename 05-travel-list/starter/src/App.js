5e9e41747c4899f2daa122b04c5ebcaca7029d24
unknown <polibos6366@gmail.com>
2024-09-30 19:55:43 -0600

    Sorting Items

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -84,10 +84,32 @@ function Form({ handleAddItems }) {
 }
 
 function PackingList({ items, handleDeleteItem, handleToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  // derived state
+
+  let sortedItems;
+
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description)); // Corrected here
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -96,6 +118,14 @@ function PackingList({ items, handleDeleteItem, handleToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort By Input order</option>
+          <option value="description">Sort By description</option>
+          <option value="packed">Sort By Packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
