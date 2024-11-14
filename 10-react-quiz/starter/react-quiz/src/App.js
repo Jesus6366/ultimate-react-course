be2b4f757e3a8c6d91f26ed500d7bd3fe5fb462f
unknown <polibos6366@gmail.com>
2024-11-13 17:58:31 -0700

    Starting a New Quiz

--- 10-react-quiz/starter/react-quiz/src/App.js
+++ 10-react-quiz/starter/react-quiz/src/App.js
@@ -4,6 +4,7 @@ import Loader from "./Loader";
 import Main from "./Main";
 import { useEffect, useReducer } from "react";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -26,6 +27,8 @@ function reducer(state, action) {
         ...state,
         status: "error",
       };
+    case "start":
+      return { ...state, status: "active" };
 
     default:
       throw new Error("Action unknown");
@@ -49,7 +52,10 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
