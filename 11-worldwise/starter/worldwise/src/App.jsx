b8a19bac37f7a640be27510ecbb0968d0cc35fbb
unknown <polibos6366@gmail.com>
2024-11-19 18:32:59 -0700

    Implementing Main Pages and Routes

--- 11-worldwise/starter/worldwise/src/App.jsx
+++ 11-worldwise/starter/worldwise/src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFoung from "./pages/PageNotFoung";
+
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFoung />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ 11-worldwise/starter/worldwise/src/pages/PageNotFoung.jsx
@@ -0,0 +1,5 @@
+const PageNotFoung = () => {
+  return <div>Page Not Foung</div>;
+};
+
+export default PageNotFoung;
