fcd41e6b9d6c413d690c32581f0808c9f0f0bc1d
unknown <polibos6366@gmail.com>
2024-11-26 18:10:49 -0700

    Reading and Setting a Query string

--- 11-worldwise/starter/worldwise/src/components/Map.jsx
+++ 11-worldwise/starter/worldwise/src/components/Map.jsx
@@ -2,10 +2,20 @@ import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 const Map = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // to read the queries
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  setSearchParams;
+
   return (
     <div className={styles.mapContainer}>
       <h1>Map</h1>
-      <h1>{/* Position: {lat},{lng} */}</h1>
+      <h1>
+        Position: {lat},{lng}
+      </h1>
     </div>
   );
 };
