f53efe81ceb0cd6746613848e498fd917db55f66
unknown <polibos6366@gmail.com>
2024-11-27 17:43:37 -0700

    Programmatic Navigation with useNavigate

--- 11-worldwise/starter/worldwise/src/components/Map.jsx
+++ 11-worldwise/starter/worldwise/src/components/Map.jsx
@@ -1,7 +1,8 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 const Map = () => {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   // to read the queries
@@ -11,7 +12,7 @@ const Map = () => {
   setSearchParams;
 
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>Map</h1>
       <h1>
         Position: {lat},{lng}
