efa8dbc57b994fb4c470ed0108bd0192d7dd3261
unknown <polibos6366@gmail.com>
2024-12-15 10:04:12 -0700

    Advanced State Management System: Context + useReducer

--- 11-worldwise/starter/worldwise/src/contexts/CitiesContext.jsx
+++ 11-worldwise/starter/worldwise/src/contexts/CitiesContext.jsx
@@ -93,6 +93,7 @@ function CitiesProvider({ children }) {
   }, []);
 
   async function getCity(id) {
+    if (Number(id) === currentCity.id) return;
     dispatch({ type: "loading" });
     try {
       const rest = await fetch(`${BASE_URL}/cities/${id}`);
